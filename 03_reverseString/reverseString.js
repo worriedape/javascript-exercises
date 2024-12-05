09c7cbd847c6182373675fe34eeae57721a95f5f
worriedape <144045807+worriedape@users.noreply.github.com>
2024-12-05 17:40:08 -0600

    Add return value of array.reverse

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,16 +1,8 @@
-const reverseString = (string) => {
-  const stringToArray = string.split("");
+const reverseString = (word) => {
+  const wordToArray = word.split("");
 
-  console.log(stringToArray);
+  return wordToArray.reverse();
 };
 
 // Do not edit below this line
 module.exports = reverseString;
-
-/*
-let string = prompt("Add a word");
-
-const stringToArray = string.split("");
-
-console.log(stringToArray)
-*/
