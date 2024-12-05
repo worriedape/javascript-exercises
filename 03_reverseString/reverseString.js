7ed0dca09483a93ed571ea7708a54eabe98b5a20
worriedape <144045807+worriedape@users.noreply.github.com>
2024-12-05 17:07:32 -0600

    Add variable stringToArray

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,16 @@
-const reverseString = function() {
+const reverseString = (string) => {
+  const stringToArray = string.split("");
 
+  console.log(stringToArray);
 };
 
 // Do not edit below this line
 module.exports = reverseString;
+
+/*
+let string = prompt("Add a word");
+
+const stringToArray = string.split("");
+
+console.log(stringToArray)
+*/
