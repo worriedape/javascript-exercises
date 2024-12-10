a4c362d66becc255f92cf565db9c658927124ad6
worriedape <144045807+worriedape@users.noreply.github.com>
2024-12-10 12:50:34 -0600

    Refactor checking conditions to pass all tests

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,10 +1,9 @@
 const leapYears = function(year) {
 
-    if ( year % 4 === 0 ||
-        year % 400 === 0 &&
-        year % 100 !== 0) {
-            return true
-        } return false
+    if ((year % 4 === 0 && year % 100 !== 0) || year % 400 === 0) {
+      return true;
+    }
+    return false;
 
 };
 
