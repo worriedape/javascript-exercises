57d134fe34601615300ec6c83915d4c1b8068583
worriedape <144045807+worriedape@users.noreply.github.com>
2024-12-10 16:38:10 -0600

    Add formulas for both conversion functions

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,17 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahre) {
+
+let convert = ((fahre - 32) * 5/9);
+
+return convert.toFixed(1)
+
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+
+let convert = ((celsius - 32) * 9 / 5);
+
+return convert.toFixed(1);
+
 };
 
 // Do not edit below this line
