5ff73d5fe9a9440d53eddbf8e8a78bbaa9437568
worriedape <144045807+worriedape@users.noreply.github.com>
2024-12-10 16:57:42 -0600

    Fix typo in celsius to fahre and pass all tests

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -2,16 +2,17 @@ const convertToCelsius = function(fahre) {
 
 let convert = ((fahre - 32) * 5/9);
 
-return convert.toFixed(1)
+//return convert.toFixed(1)
 
+return Math.round(convert * 10) / 10;
 };
 
 const convertToFahrenheit = function(celsius) {
 
-let convert = ((celsius - 32) * 9 / 5);
+let convert = ((celsius * 9/5) + 32);
 
-return convert.toFixed(1);
-
+//return convert.toFixed(1);
+return Math.round(convert * 10) / 10;
 };
 
 // Do not edit below this line
