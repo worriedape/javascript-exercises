f35be2dbb4598fcf73f65bac73fcad0692ce79af
worriedape <144045807+worriedape@users.noreply.github.com>
2024-12-23 18:43:37 -0600

    Complete solution and passes all tests

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,18 @@
-const palindromes = function () {
+const palindromes = function (string) {
+  let normal = string
+    .toLowerCase()
+    .split("")
+    .filter((item) => /^[a-z0-9]$/.test(item));
+    // defining a variables with all alphanumerical
+    // characters and checking it to item with .includes
+    // may be less computational intensive
+    // and improves readability and maintainance
 
+ let reversed = normal.slice().reverse();
+
+ if(normal.join()===reversed.join()) {
+    return true;
+ } return false;
 };
 
 // Do not edit below this line
