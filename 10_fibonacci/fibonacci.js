3366f10135649129988fd817545bb48ba92d803a
worriedape <144045807+worriedape@users.noreply.github.com>
2024-12-23 21:40:33 -0600

    Complete fibonacci function to pass all tests

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,11 @@
-const fibonacci = function() {
+const fibonacci = function (num) {
+  if(num<0) return "OOPS"
+  let arr = [0, 1];
 
+  while (arr.length <= num) {
+    arr.push(arr[arr.length - 1] + arr[arr.length - 2]);
+  }
+  return arr[num];
 };
 
 // Do not edit below this line
