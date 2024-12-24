0ba2dc7d9884446cdefffaa93a6bb5270ef86dc1
worriedape <144045807+worriedape@users.noreply.github.com>
2024-12-23 20:47:32 -0600

    Complete findTheOldest function and pass all tests

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,31 @@
-const findTheOldest = function() {
-
+const findTheOldest = function (collection) {
+ collection.sort(
+   (a, b) =>
+     ((b.yearOfDeath || new Date().getFullYear()) -
+     b.yearOfBirth) -
+     ((a.yearOfDeath || new Date().getFullYear()) - a.yearOfBirth)
+ );
+ return collection[0];
 };
 
 // Do not edit below this line
 module.exports = findTheOldest;
+
+ // let years = collection.yearOfDeath - collection.yearOfBirth
+  //return collection.sort((a, b)=> )
+
+  
+
+ /*let sortYears;
+
+  if (!collection.yearOfDeath === undefined) {
+    sortYears = collection.sort(
+      (a, b) => (a.yearOfDeath - a.yearOfBirth) - (b.yearOfDeath - b.yearOfBirth)
+    );
+  } else if(collection.yearOfDeath === undefined) {
+    sortYears = collection.sort(
+      (a, b) => (((new Date()).getFullYear()) - a.yearOfBirth) - (((new Date()).getFullYear()) - b.yearOfDeath)
+    );
+  }
+
+  return sortYears;*/
